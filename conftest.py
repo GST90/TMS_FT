c778b38aa048f3669c2a2edc3621590792fdcf74
GST90 <78861010+GST90@users.noreply.github.com>
2021-10-01 00:46:07 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,12 +6,11 @@ from helpers.database import DB
 
 @pytest.fixture()
 def browser():
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-gpu')
-    # browser = webdriver.Chrome(chrome_options=chrome_options)
-    browser = webdriver.Chrome("C:\chromedriver.exe")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.maximize_window()
     browser.implicitly_wait(10)
     yield browser
