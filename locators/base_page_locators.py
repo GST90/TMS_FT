0ec7083cd32805e2040cbf9246b792cf00c4f19d
GST90 <78861010+GST90@users.noreply.github.com>
2021-10-05 14:34:15 +0300

    Update base_page_locators.py

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
 
     LOCATOR_OPEN_CHANGES = (By.CLASS_NAME, 'fancybox-region')
-    LOCATOR_CURRENCY_APPEARANCE = (By.XPATH, '//*[@id="region"]/div[2]')
-    LOCATOR_COUNTRY_APPEARANCE = (By.XPATH, '//*[@id="region"]/div[3]')
+    LOCATOR_CURRENCY_APPEARANCE = (By.XPATH, '//*[@class="currency"]')
+    LOCATOR_COUNTRY_APPEARANCE = (By.XPATH, '//*[@class="country"]')
     LOCATOR_LOGIN = (By.XPATH, '//input[@name="email"]')
     LOCATOR_PASSWORD = (By.XPATH, '//input[@name="password"]')
     LOCATOR_LOGIN_BUTTON = (By.XPATH, '//button[@name="login"]')
