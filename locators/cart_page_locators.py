a74c7cc3a9b946cc0be5f13b2aba9b865bc4c8f3
GST90 <78861010+GST90@users.noreply.github.com>
2021-10-05 14:33:34 +0300

    Update cart_page_locators.py

--- locators/cart_page_locators.py
+++ locators/cart_page_locators.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 class CartPageLocators:
     LOCATOR_GOODS_TITLE = (By.XPATH, '//strong[text()="Red Duck"]')
     LOCATOR_UNIT_PRICE = (By.XPATH, '//tbody//td[@class="unit-cost"]')
-    LOCATOR_TOTAL_PRICE = (By.XPATH, '(//tr[@class="footer"]//strong)[2]')
+    LOCATOR_TOTAL_PRICE = (By.XPATH, '//tbody//td[@class="sum"]')
     LOCATOR_CONFIRM_ORDER = (By.XPATH, '//button[@name="confirm_order"]')
