73c537958a42fec72ed76da669ff60c709a8a513
GST90 <78861010+GST90@users.noreply.github.com>
2021-10-05 14:34:43 +0300

    Update main_page_locators.py

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -5,4 +5,4 @@ class MainPageLocators:
     GOODS_PRICE = (By.XPATH, '//span[@class="price"]')
     LOCATOR_QUANTITY = (By.XPATH, '//input[@name="quantity"]')
     LOCATOR_ADD_TO_CART = (By.XPATH, '//*[@name="add_cart_product"]')
-    LOCATOR_CART_BUTTON = (By.XPATH, '//*[@id="cart"]/a[3]')
+    LOCATOR_CART_BUTTON = (By.XPATH, '//*[@id="cart"]')
